b2b05c1455d811a77bd1c0e303b69f630900e7a6
abbas-ali <abbasalimd@gmail.com>
2014-05-24 22:16:28 -0400

    My first attempt
    At submitting a r program via git

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,33 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+## Makes a matrix from random numbers
+makeCacheMatrix <- function(x=matrix(rnorm(4), 2,2)) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+## Return a matrix that is the inverse of 'x'
+        get <- function() x
+        setmean <- function(solve) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+##Function checks to see if the inverse is already calculated if not it calculates the inverse
+
+cacheSolve <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmean(m)
+        m
+}
